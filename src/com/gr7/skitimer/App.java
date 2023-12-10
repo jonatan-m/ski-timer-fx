f9d9ea6a078d5c220db47c32065e0cfad471b320
LarsRydberg <153441378+LarsRydberg@users.noreply.github.com>
2023-12-10 17:20:28 +0100

    Update App.java

--- src/com/gr7/skitimer/App.java
+++ src/com/gr7/skitimer/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Hello World!!");
         Button btn = new Button();
         btn.setText("Say 'Tjena världen'");
         btn.setOnAction(new EventHandler<ActionEvent>() {

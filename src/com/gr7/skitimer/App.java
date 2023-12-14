a0ecb1a2f7908c6b9e97f70ed2025bd8de370f9f
jonatan-m <jonatan.malander@gmail.com>
2023-12-14 22:27:03 +0100

    Testing some stuff

--- src/com/gr7/skitimer/App.java
+++ src/com/gr7/skitimer/App.java
@@ -1,5 +1,8 @@
 package com.gr7.skitimer;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,7 +12,17 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class App extends Application {
+	static Competition comp = new Competition(StartFormat.INTERVAL);
+	
     public static void main(String[] args) {
+    	comp.addCompetitor("Shakur", "04");
+    	comp.addCompetitor("Fredde", "02");
+    	comp.addCompetitor("Jonte", "01");
+    	comp.addCompetitor("Lars", "03");
+    	
+    	comp.setStartTimes(LocalTime.of(10, 0, 0));
+    	
+   
         launch(args);
     }
     
@@ -17,12 +30,16 @@ public class App extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!!");
         Button btn = new Button();
-        btn.setText("Say 'Tjena världen'");
+        btn.setText("Ta tid åkare 01");
         btn.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Tjena världen!");
+            	Competitor gunde = comp.getCompetitors().get("01");
+            	long seconds = gunde.getStartTime().until(LocalTime.now(), ChronoUnit.SECONDS);
+            	
+            	var time = LocalTime.ofSecondOfDay(seconds);
+                System.out.println(time.toString().formatted("hh:mm:ss"));
             }
         });
         

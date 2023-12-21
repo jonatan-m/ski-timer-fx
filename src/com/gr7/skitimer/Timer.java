4a261b0a386eaf239f718e03367b6aee0a1205c8
LarsRydberg <153441378+LarsRydberg@users.noreply.github.com>
2023-12-21 09:51:18 +0100

    Update Timer.java

--- src/com/gr7/skitimer/Timer.java
+++ src/com/gr7/skitimer/Timer.java
@@ -13,12 +13,8 @@ class Timer {
 		LocalTime time = LocalTime.now();
         Duration timeElapsed = Duration.between(startTime, time);
         
-        long seckonds = timeElapsed.getSeconds();
-        int hours = (int) seckonds / 3600;
-        int minuts = ((int) seckonds %3600) / 60;
-        int sec = ((int) seckonds/3600) % 60;
-        int nanoSeconds = timeElapsed.getNano();
-        LocalTime results = LocalTime.of(hours, minuts, sec, nanoSeconds);
+        LocalTime results = LocalTime.ofNanoOfDay(timeElapsed.getNano());
+        
         return results;      
 	}
 

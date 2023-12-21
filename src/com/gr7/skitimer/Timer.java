a28a83aeade63fec90e2f212ba9db8cc81271e77
LarsRydberg <153441378+LarsRydberg@users.noreply.github.com>
2023-12-21 09:43:16 +0100

    Update Timer.java

--- src/com/gr7/skitimer/Timer.java
+++ src/com/gr7/skitimer/Timer.java
@@ -5,23 +5,25 @@ import java.time.LocalTime;
 
 
 class Timer {
-	private static LocalTime time = LocalTime.now();
 	
 	
-	Timer(){
-		
-	};
+	Timer(){};
 	
-	public static Duration stopTimer(LocalTime startTime) {
-		
+	public static LocalTime stopTimer(LocalTime startTime) {
+		LocalTime time = LocalTime.now();
         Duration timeElapsed = Duration.between(startTime, time);
-         
-        return timeElapsed;
         
+        long seckonds = timeElapsed.getSeconds();
+        int hours = (int) seckonds / 3600;
+        int minuts = ((int) seckonds %3600) / 60;
+        int sec = ((int) seckonds/3600) % 60;
+        int nanoSeconds = timeElapsed.getNano();
+        LocalTime results = LocalTime.of(hours, minuts, sec, nanoSeconds);
+        return results;      
 	}
 
 	public LocalTime getTime() {
-		return time;
+		return LocalTime.now();
 	}
 	
 	
